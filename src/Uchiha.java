201ed57f438fd5ae3dde139b6f8282220268c2bb
Johnny Kava <johnnylk2003@hotmail.com>
2025-03-29 12:12:42 -0300

    fix construtor

--- src/Uchiha.java
+++ src/Uchiha.java
@@ -1,11 +1,22 @@
 public class Uchiha extends Ninja{
     String habilidadeEspecial = "Sharingan";
 
-    public Uchiha(String nome, int idade, String missao, char nivelDificuldade, String statusMissao) {
+    public Uchiha(String nome, int idade, String missao, String nivelDificuldade, String statusMissao) {
         super(nome, idade, missao, nivelDificuldade, statusMissao);
     }
 
     public void mostrarHabilidadeEspecial(){
         System.out.println("Habilidade Especial: " + habilidadeEspecial);
+    }
+
+    @Override
+    public void mostrarInformacoes() {
+        System.out.println("----------------- INFO NINJA -----------------");
+        System.out.println("Nome: " + this.nome + " Uchiha");
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Missão: " + this.missao);
+        System.out.println("Nivel da missão: " + this.nivelDificuldade);
+        System.out.println("Status da missão: " + this.statusMissao);
+        System.out.println("----------------------------------------------");
     }
 }
