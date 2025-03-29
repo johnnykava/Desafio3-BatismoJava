eb946e706c80072137a24a3a03ecb4282c112f9d
Johnny Kava <johnnylk2003@hotmail.com>
2025-03-29 11:39:52 -0300

    add contrutor na classe ninja, mais metodo para mostrar informações

--- src/Ninja.java
+++ src/Ninja.java
@@ -4,4 +4,22 @@ public class Ninja {
     String missao;
     char nivelDificuldade;
     String statusMissao;
+
+    public Ninja(String nome, int idade, String missao, char nivelDificuldade, String statusMissao){
+        this.nome = nome;
+        this.idade = idade;
+        this.missao = missao;
+        this.nivelDificuldade = nivelDificuldade;
+        this.statusMissao = statusMissao;
+    }
+
+    public void mostrarInformacoes(){
+        System.out.println("----------------- INFO NINJA -----------------");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Missão: " + this.missao);
+        System.out.println("Nivel da missão: " + this.nivelDificuldade);
+        System.out.println("Status da missão: " + this.statusMissao);
+        System.out.println("----------------------------------------------");
+    }
 }
