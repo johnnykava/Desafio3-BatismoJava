d0bebde25c3275d8a8632ddf161a70cfee49efa6
Johnny Kava <johnnylk2003@hotmail.com>
2025-03-29 12:18:40 -0300

    add getters and setters

--- src/Ninja.java
+++ src/Ninja.java
@@ -1,9 +1,49 @@
 public class Ninja {
-    String nome;
-    int idade;
-    String missao;
-    String nivelDificuldade;
-    String statusMissao;
+    private String nome;
+    private int idade;
+    private String missao;
+    private String nivelDificuldade;
+    private String statusMissao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMissao() {
+        return missao;
+    }
+
+    public void setMissao(String missao) {
+        this.missao = missao;
+    }
+
+    public String getNivelDificuldade() {
+        return nivelDificuldade;
+    }
+
+    public void setNivelDificuldade(String nivelDificuldade) {
+        this.nivelDificuldade = nivelDificuldade;
+    }
+
+    public String getStatusMissao() {
+        return statusMissao;
+    }
+
+    public void setStatusMissao(String statusMissao) {
+        this.statusMissao = statusMissao;
+    }
 
     public Ninja(String nome, int idade, String missao, String nivelDificuldade, String statusMissao){
         this.nome = nome;
