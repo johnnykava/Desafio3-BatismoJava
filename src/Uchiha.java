5b87e860e36c7620fe2a7cf920dd83908aab60ad
Johnny Kava <johnnylk2003@hotmail.com>
2025-03-29 12:49:06 -0300

    add getters and setters na classe Uchiha

--- src/Uchiha.java
+++ src/Uchiha.java
@@ -1,5 +1,13 @@
 public class Uchiha extends Ninja{
-    String habilidadeEspecial = "Sharingan";
+    private String habilidadeEspecial = "Sharingan";
+
+    public String getHabilidadeEspecial() {
+        return habilidadeEspecial;
+    }
+
+    public void setHabilidadeEspecial(String habilidadeEspecial) {
+        this.habilidadeEspecial = habilidadeEspecial;
+    }
 
     public Uchiha(String nome, int idade, String missao, String nivelDificuldade, String statusMissao) {
         super(nome, idade, missao, nivelDificuldade, statusMissao);
@@ -14,6 +22,7 @@ public class Uchiha extends Ninja{
         System.out.println("----------------- INFO NINJA -----------------");
         System.out.println("Nome: " + getNome() + " Uchiha");
         System.out.println("Idade: " + getIdade());
+        System.out.println("Habilidade Especial: " + getHabilidadeEspecial());
         System.out.println("Missão: " + getMissao());
         System.out.println("Nivel da missão: " + getNivelDificuldade());
         System.out.println("Status da missão: " + getStatusMissao());
